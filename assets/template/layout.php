b4c9258fe421ad67183ddcdf4592da59bbbf0528
Jeanne <jeanne.tiramani@hotmail.fr>
2017-09-20 12:36:01 +0200

    navbar

--- assets/template/layout.php
+++ assets/template/layout.php
@@ -2,9 +2,25 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
 	<title><?= $title ?></title>
 </head>
 <body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+		<div class="navbar-header">
+			<a href="?p=home" class="navbar-brand"><i class="glyphicon glyphicon-home"></i><a>
+		</div>
+			
+			<form class="navbar-form navbar-right" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+		</div>
+	</nav>
+
 	<?= $content ?>
 </body>
 </html>
