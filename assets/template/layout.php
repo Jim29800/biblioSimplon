49c40a6786f4bbd5a33bdaa74c5a8dbb8530dbfa
Jeanne <jeanne.tiramani@hotmail.fr>
2017-09-20 14:01:22 +0200

    footer

--- assets/template/layout.php
+++ assets/template/layout.php
@@ -6,21 +6,40 @@
 	<title><?= $title ?></title>
 </head>
 <body>
-	<nav class="navbar navbar-default navbar-fixed-top">
-		<div class="container">
-		<div class="navbar-header">
-			<a href="?p=home" class="navbar-brand"><i class="glyphicon glyphicon-home"></i><a>
-		</div>
-			
-			<form class="navbar-form navbar-right" role="search">
-				<div class="form-group">
-					<input type="text" class="form-control" placeholder="Search">
+	<header>
+		<nav class="navbar navbar-default">
+			<div class="container">
+				<div class="navbar-header">
+					<a href="?p=home" class="navbar-brand"><i class="glyphicon glyphicon-home"></i></a>
 				</div>
-				<button type="submit" class="btn btn-default">Submit</button>
-			</form>
-		</div>
-	</nav>
-
-	<?= $content ?>
-</body>
-</html>
+				
+				<form class="navbar-form navbar-right" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search
+						"></i></button>
+					</form>
+				</div>
+			</nav>
+		</header>
+		<section class="container">
+			<?= $content ?>
+		</section>
+		<footer>
+			<div class="container">
+				<ul class="list-inline ">
+					<li class=" col-md-offset-1 list-inline-item">
+						Créé par : Bouvier Jimmy - Cheurfa Nassim - Tiramani Jeanne
+					</li>
+					<li class=" col-md-offset-1 list-inline-item">
+						Organisation : Simplon Carcassonne
+					</li>
+					<li class=" col-md-offset-1 list-inline-item">
+						2017
+					</li>
+				</ul>
+			</div>
+		</footer>
+	</body>
+	</html>
--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,3 @@
-<h1>OKAY</h1>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem ullam veritatis, fugit minima sequi officia aut quis tenetur modi eius, dolor fuga explicabo non commodi quaerat est excepturi! Deleniti, nesciunt?
+</p>
