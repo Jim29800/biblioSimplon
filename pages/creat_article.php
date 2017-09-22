85590cedac0a8cbe4a07106964f71c4f8af61802
Jim29800 <jbouvier1173@gmail.com>
2017-09-22 11:53:24 +0200

    Ajout d'article dans la BDD

--- pages/creat_article.php
+++ pages/creat_article.php
@@ -2,39 +2,51 @@
 //Traitement PHP
 if(!empty($_POST)):
     $titre = htmlspecialchars($_POST['titre']);
-    $genre = htmlspecialchars($_POST['genre']);
+    $genre = htmlspecialchars((INT)$_POST['genre']);
     $contenu = htmlspecialchars($_POST['contenu']);
     $auteur = htmlspecialchars($_POST['auteur']);
     $date = htmlspecialchars($_POST['date']);
-    $sql_ajout_article = sprintf("INSERT INTO art_article (art_titre,art_content,art_auteur,art_date)VALUES 
-                                '%s','%s','%s','%s'", $titre, $contenu, $auteur, $date);
-
-
+//ajoute les articles
+    $sql_ajout_article = sprintf("INSERT INTO art_article (art_titre,art_content,art_auteur,art_date, art_gnr_oid)VALUES 
+                                ('%s','%s','%s','%s', %d)", $titre, $contenu, $auteur, $date, $genre);
+    try {
+        $bdd->query($sql_ajout_article);
+        $succes = "L'article suivant est enregistré : ".$titre;
+    } catch (Exception $e) {
+        echo $e->getMessage(), "\n";
+    }
 endif;
-    // art_titre varchar(50),
-	// art_content text,
-	// art_auteur varchar(50),
-    // art_date varchar(20),
-        $sql_genre = $bdd->query(sprintf("SELECT * FROM gnr_genre"));
+//requete la liste des genres
+try {
+    $sql_genre = $bdd->query(sprintf("SELECT * FROM gnr_genre ORDER BY gnr_libele asc"));
+} catch (Exception $e) {
+    echo $e->getMessage(), "\n";
+}
 ?>
 <!-- Affichage HTML -->
+<?php
+    if (!empty($succes)){
+        echo "<h1>".$succes."</h1>";
+        $succes = "";
+    }
+?>
 <form action="" method="post" class="text-center">
 <ul class="list-unstyled">
     <li><input type="submit" value="Créer l'article"></li><br/>
     <li class="form-inline">
         <div class="form-group">
-            <input type="text" name="titre" id="titre" placeholder="Titre">
+            <input require type="text" name="titre" id="titre" placeholder="Titre">
             <input type="text" name="auteur" id="auteur" placeholder="Auteur">
             <select name="genre" id="genre" class="form-control">
-                <optgroup label="Genre" >
+                <optgroup label="Genre">
                     <?php foreach ($sql_genre as $key => $value) :?>
-                        <option val= <?= $value["gnr_oid"] ?>> <?= $value["gnr_libele"] ?> </option>
+                        <option value= <?= $value["gnr_oid"] ?>> <?= $value["gnr_libele"] ?> </option>
                     <?php endforeach ?>
                 </optgroup>
             </select>
-            <input type="date" name="" id="" placeholder="">
+            <input type="date" name="date" id="date">
         </div>
     </li><br/>
-    <li><textarea name="" id="" cols="100" rows="30"></textarea></li>
+    <li><textarea name="contenu" id="contenu" cols="100" rows="30"></textarea></li>
 </ul>
 </form>
